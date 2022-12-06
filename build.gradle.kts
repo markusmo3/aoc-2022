dea16ce1fbbf573c50537bb48da038c7adaedbf6
Markus Moser <m.moser@reply.de>
2022-12-06 16:54:03 +0100

    Initialize aoc with some more helper methods and source file generation

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,3 +17,18 @@ tasks {
         gradleVersion = "7.6"
     }
 }
+
+tasks.register("generateSourceFiles") {
+    doLast {
+        val sourceTemplateText = file(layout.projectDirectory.file("src/Day00_template.kt")).readText()
+        for (i in 1..24) {
+            val iFormatted = if (i < 10) "0$i" else "$i"
+            val sourceFile = file(layout.projectDirectory.file("src/Day${iFormatted}.kt"))
+            sourceFile.writeText(sourceTemplateText)
+            val inputFile = file(layout.projectDirectory.file("src/Day${iFormatted}_input.txt"))
+            inputFile.writeText("")
+            val testFile = file(layout.projectDirectory.file("src/Day${iFormatted}_test.txt"))
+            testFile.writeText("")
+        }
+    }
+}
--- src/Utils.kt
+++ src/Utils.kt
@@ -2,6 +2,22 @@ import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
 
+fun readTestInput(): List<String> {
+    var callerClass = Thread.currentThread().getStackTrace()[2].getClassName()
+    if (callerClass.contains("_")) {
+        callerClass = callerClass.substring(0, callerClass.indexOf('_'))
+    }
+    return File("src", "${callerClass}_test.txt").readLines()
+}
+
+fun readInput(): List<String> {
+    var callerClass = Thread.currentThread().getStackTrace()[2].getClassName()
+    if (callerClass.contains("_")) {
+        callerClass = callerClass.substring(0, callerClass.indexOf('_'))
+    }
+    return File("src", "${callerClass}_input.txt").readLines()
+}
+
 /**
  * Reads lines from the given input txt file.
  */
@@ -11,6 +27,7 @@ fun readInput(name: String) = File("src", "$name.txt")
 /**
  * Converts string to md5 hash.
  */
-fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
+fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5")
+    .digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
