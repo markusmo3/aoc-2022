1b99d2a6864f56d0df018239d7a4d74f01ed1a78
Markus Moser <m.moser@reply.de>
2022-12-08 00:11:51 +0100

    finish Day04

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,19 +1,38 @@
-@file:Suppress("UNUSED_PARAMETER", "DuplicatedCode")
+@file:Suppress("UNUSED_PARAMETER", "DuplicatedCode", "KotlinRedundantDiagnosticSuppress")
 
 fun main() {
     fun part1(input: List<String>): String {
-        return ""
+        var count = 0
+        for (s in input) {
+            val split = s.split(",", "-").map { it.toInt() }
+            if (split[0] >= split[2] && split[1] <= split[3] ||
+                split[2] >= split[0] && split[3] <= split[1]) {
+                count++
+            }
+        }
+        return "$count"
     }
 
     fun part2(input: List<String>): String {
-        return ""
+        var count = 0
+        for (s in input) {
+            val split = s.split(",", "-").map { it.toInt() }
+            if (split[0] <= split[2] && split[2] <= split[1] ||
+                split[0] <= split[3] && split[3] <= split[1] ||
+                split[2] <= split[0] && split[0] <= split[3] ||
+                split[2] <= split[1] && split[1] <= split[3]
+            ) {
+                count++
+            }
+        }
+        return "$count"
     }
 
 
     println("================ ${getCallerClass()} =================")
-    checkEquals("", part1(readTestInput()))
+    checkEquals("2", part1(readTestInput()))
     println("=> Part1: " + part1(readInput()))
     println("========================================")
-    checkEquals("", part2(readTestInput()))
+    checkEquals("4", part2(readTestInput()))
     println("=> Part2: " + part2(readInput()))
 }
