b3eea46643652360387fcb1c11da032616d5427e
Markus Moser <m.moser@reply.de>
2022-12-07 12:06:51 +0100

    finish Day07

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,19 +1,95 @@
-@file:Suppress("UNUSED_PARAMETER", "DuplicatedCode")
+@file:Suppress("UNUSED_PARAMETER", "DuplicatedCode", "KotlinRedundantDiagnosticSuppress")
 
 fun main() {
+    fun propagateSize(
+        curDir: String,
+        dirs: MutableMap<String, Long>,
+        curSize: Long
+    ) {
+        if (curDir == "/") {
+            dirs.merge("/", curSize) { i, j -> i + j }
+            return
+        }
+
+        var propagateDir = curDir
+        while (propagateDir != "") {
+            dirs.merge(propagateDir, curSize) { i, j -> i + j }
+            propagateDir = propagateDir.substring(0, propagateDir.lastIndexOf('/'))
+            if (propagateDir == "") {
+                // reached the root
+                dirs.merge("/", curSize) { i, j -> i + j }
+                break
+            }
+        }
+    }
+
+    fun parseDirSizes(input: List<String>): MutableMap<String, Long> {
+        val dirs = mutableMapOf<String, Long>()
+        var curDir = ""
+        var curSize: Long = 0
+        for (s in input) {
+            if (s.startsWith("$") && curSize > 0) {
+                propagateSize(curDir, dirs, curSize)
+                curSize = 0
+            }
+
+            if (s.startsWith("$ cd")) {
+                val newDir = s.substring(5)
+                if (newDir == "/") {
+                    curDir = "/"
+                } else if (newDir == "..") {
+                    curDir = curDir.substring(0, curDir.lastIndexOf('/'))
+                } else {
+                    if (curDir == "/") {
+                        curDir += newDir
+                    } else {
+                        curDir += "/$newDir"
+                    }
+                }
+            } else if (s.startsWith("$ ls")) {
+                // NOOP
+            } else if (s.startsWith("dir")) {
+                // NOOP
+            } else {
+                val fileSize = s.split(' ')[0].toInt()
+                curSize += fileSize
+            }
+        }
+        if (curSize > 0) {
+            propagateSize(curDir, dirs, curSize)
+        }
+        return dirs
+    }
+
     fun part1(input: List<String>): String {
-        return ""
+        val dirs = parseDirSizes(input)
+
+        dirs.toSortedMap().forEach { t, u -> println("$t=$u") }
+        return dirs.filterValues { it <= 100000 }.values.sum().toString()
     }
 
     fun part2(input: List<String>): String {
-        return ""
+        // total disk 70000000
+        // need at least 30000000
+
+        val dirs = parseDirSizes(input)
+        val currentUnusedSize = 70_000_000 - dirs["/"]!!
+        val sizeNeededForDeletion = 30_000_000 - currentUnusedSize
+        println("rootSize=${dirs["/"]}")
+        println("currentUnusedSize=$currentUnusedSize")
+        println("sizeNeededForDeletion=$sizeNeededForDeletion")
+//        dirs.toSortedMap().forEach { t, u -> println("$t=$u") }
+        val sortedByValue =
+            dirs.filterValues { it > sizeNeededForDeletion }.entries.sortedBy { it.value }
+        sortedByValue.forEach{ e -> println(e) }
+        return dirs.filterValues { it > sizeNeededForDeletion }.minBy { it.value }.toString()
     }
 
 
     println("================ ${getCallerClass()} =================")
-    checkEquals("", part1(readTestInput()))
+    checkEquals("95437", part1(readTestInput()))
     println("=> Part1: " + part1(readInput()))
     println("========================================")
-    checkEquals("", part2(readTestInput()))
+    checkEquals("/d=24933642", part2(readTestInput()))
     println("=> Part2: " + part2(readInput()))
 }
