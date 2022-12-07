720550c5e201097b1b741f45e1c3e902cab4fd19
Markus Moser <m.moser@reply.de>
2022-12-07 14:10:40 +0100

    finish Day06

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,19 +1,28 @@
-@file:Suppress("UNUSED_PARAMETER", "DuplicatedCode")
+@file:Suppress("UNUSED_PARAMETER", "DuplicatedCode", "KotlinRedundantDiagnosticSuppress")
 
 fun main() {
+    fun firstAfterWindow(s: String, windowSize: Int): Int {
+        val first = s.toCharArray().mapIndexed { index, c -> Pair(index, c) }
+            .windowed(windowSize, partialWindows = true).first {
+                it.map { it.second }.distinct().size == windowSize
+            }
+        println(first.toString())
+        return first[0].first + windowSize
+    }
+
     fun part1(input: List<String>): String {
-        return ""
+        return firstAfterWindow(input[0], 4).toString()
     }
 
     fun part2(input: List<String>): String {
-        return ""
+        return firstAfterWindow(input[0], 14).toString()
     }
 
 
     println("================ ${getCallerClass()} =================")
-    checkEquals("", part1(readTestInput()))
+    checkEquals("11", part1(readTestInput()))
     println("=> Part1: " + part1(readInput()))
     println("========================================")
-    checkEquals("", part2(readTestInput()))
+    checkEquals("26", part2(readTestInput()))
     println("=> Part2: " + part2(readInput()))
 }
